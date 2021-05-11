279ae638409ce0a25cf65f882e0c5e4f8aca7fb6
b50905du <a0905638606@gmail.com>
2021-05-11 16:03:27 +0800

    123

---
+++ AIIIIIIIII/Assets/SCRIPTS/Bomping.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Bomping : MonoBehaviour
+{
+    public Rigidbody rigid;
+
+    public Vector3 force;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag=="Floor") 
+        {
+            rigid.AddForce(force);
+        }
+    }
+}
